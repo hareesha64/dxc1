d36cd1be09dae13759d994a85bd3cca398698f4c
hareesha <harisha21.p@gmail.com>
2020-09-29 04:33:58 -0700

    adding assign1

---
+++ Employee.java
@@ -0,0 +1,26 @@
+//making change sin branches1
+public class Employee {
+		int id;
+		String name;
+		double sal;
+		Employee()
+		{
+			
+		}
+		Employee(int eid,String ename,double esal)
+		{
+			this.id=eid;
+			this.name=ename;
+			this.sal=esal;
+		}
+		
+		public void showTotalSal()
+		{
+		System.out.println(sal);
+			
+		}
+		
+		
+	
+
+}
